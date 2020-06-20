22991f13bf9747011ec1babbf990117571548a22
Anderson Martins <tuco.anderson@gmail.com>
2020-06-20 14:09:20 -0300

    criação de service de customers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -13,7 +13,7 @@ interface IRequest {
 @injectable()
 class CreateCustomerService {
   constructor(
-    @inject('Customer')
+    @inject('CustomersRepository')
     private customerRepository: ICustomersRepository,
   ) {}
 
